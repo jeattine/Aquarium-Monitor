0bdf3caa7bf88df8f8dd7e90f4e19b4368fda95f
jeattine <john.attinella@gmail.com>
2026-07-04 22:59:08 -0500

    Fix name for log file
    The wrong name was being used for the log file.

--- save-logs.sh
+++ save-logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Only run this if the system is in the process of shutting down
 if [[ "$(systemctl is-system-running)" == "stopping" ]]; then
-    if [ -f /tmp/log.txt ]; then
-        rsync -a /tmp/log.txt /home/aquamon/reef_monitor/logs/log.txt
+    if [ -f /tmp/aquamon.log ]; then
+        rsync -a /tmp/aquamon.log /home/aquamon/reef_monitor/logs/aquamon.log
     fi
 fi
